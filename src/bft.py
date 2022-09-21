9dbd4fa61075d3101bcb1ddc41e9c4e917c0281e
Anton Homilius <antonhomilius@hotmail.com>
2022-09-21 20:48:52 +0200

    test

--- src/bft.py
+++ src/bft.py
@@ -1,15 +1,52 @@
 """A module for breadth-first traversal of trees."""
 
-from collections import deque
+import collections
 from typing import Iterable
 from tree import T
+        
+####################################
+# Functions:
+
+def count_nodes(tree):
+    ''' Function for counting nodes in a tree
+    
+    Example:
+    tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
+    count_nodes(tree)
+    >5
+    
+    '''
+
+    if tree == None:
+        return 0
+    else:
+        return 1 + count_nodes(tree.left) + count_nodes(tree.right)  
 
 
-def bf_order(t: T | None) -> Iterable[int]:
+def bf_order(tree):
     """Breadth-first traversal of a tree.
 
     >>> tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
     >>> list(bf_order(tree))
     [2, 1, 4, 3, 5]
     """
-    return  # FIXME
+    nodes = []
+    def visit(tree):
+        if tree == None:
+            return 0
+        if tree != None:
+            if tree.val not in nodes:
+                nodes.append(tree.val)
+        for tree in [tree.left, tree.right]:
+            if tree != None:
+                nodes.append(tree.val)
+            visit(tree)  
+    visit(tree)
+    return nodes
+
+####################################
+
+
+
+
+
