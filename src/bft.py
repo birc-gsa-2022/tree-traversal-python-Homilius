1e8e8a0e639c4c5055d4e2f68a60431638f255a8
Anton Homilius <antonhomilius@hotmail.com>
2022-09-22 14:36:38 +0200

    test2

--- src/bft.py
+++ src/bft.py
@@ -1,7 +1,8 @@
 """A module for breadth-first traversal of trees."""
 
-import collections
-from typing import Iterable
+####################################
+# Classes:
+
 from tree import T
         
 ####################################
@@ -13,40 +14,58 @@ def count_nodes(tree):
     Example:
     tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
     count_nodes(tree)
-    >5
+    5
     
     '''
-
     if tree == None:
         return 0
     else:
         return 1 + count_nodes(tree.left) + count_nodes(tree.right)  
 
 
+def visit(tree, node):
+    ''' Visits specified node
+    
+    Example:
+    tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
+    visit(tree, 4)
+    T(val=4, left=T(val=3, left=None, right=None), right=T(val=5, left=None, right=None))
+    
+    '''
+    if tree == None:
+        return None
+    if tree.val == node:
+        return tree
+    for subtree in [tree.left,tree.right]:
+        if subtree != None:
+            if subtree.val == node:
+                return subtree
+            visit(subtree, node)
+
+
 def bf_order(tree):
     """Breadth-first traversal of a tree.
-
-    >>> tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
-    >>> list(bf_order(tree))
+    
+    Example:
+    tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
+    bf_order(tree)
     [2, 1, 4, 3, 5]
+    
     """
     nodes = []
-    def visit(tree):
-        if tree == None:
-            return 0
-        if tree != None:
-            if tree.val not in nodes:
-                nodes.append(tree.val)
-        for tree in [tree.left, tree.right]:
-            if tree != None:
-                nodes.append(tree.val)
-            visit(tree)  
-    visit(tree)
+    if tree == None:
+        return []
+    if tree != None:
+        nodes.append(tree.val)
+    sub = [tree.left, tree.right]
+    for subtree in sub:
+        if subtree != None and subtree.val not in nodes:
+            nodes.append(subtree.val)
+            sub.append(subtree.left)
+            sub.append(subtree.right)
+        bf_order(subtree)
     return nodes
 
 ####################################
-
-
-
 
 
